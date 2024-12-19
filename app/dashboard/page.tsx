5362cec1a5fdc7db421b9c25bc533813a0577650
Thales Souza <santos.thales89@gmail.com>
2024-12-19 13:34:48 -0300

    commit inicial - dashboard

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,18 @@
+// import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import { lusitana } from '@/app/ui/fonts';
+
+
+export default async function Page() {
+
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        
+      </div>
+    </main>
+  );
+}
